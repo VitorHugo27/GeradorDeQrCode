476c87f74d88cc3714ea15f1b8762dbb21a82591
VitorHugo27 <88903733+VitorHugo27@users.noreply.github.com>
2023-06-18 14:34:57 -0300

    Adicionar arquivos de projeto.

---
+++ Gerador De QrCode/Controllers/GeradorQrCodeController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using QRCoder;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace Gerador_De_QrCode.Controllers
+{
+    public class GeradorQrCodeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult GerarQrCode()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult GerarQrCode(string palavra)
+        {
+            //var palavra = "Café";
+            using(MemoryStream ms = new MemoryStream())
+            {
+                QRCodeGenerator qrGenerator = new QRCodeGenerator();
+                QRCodeData qRCodeData = qrGenerator.CreateQrCode(palavra, QRCodeGenerator.ECCLevel.Q);
+                QRCode qrCode = new QRCode(qRCodeData);
+
+                using(Bitmap qrCodeBitmap = qrCode.GetGraphic(30))
+                {
+                    qrCodeBitmap.Save(ms, ImageFormat.Png);
+
+                    ViewBag.QrCodeImage = "data:image/png;base64," + Convert.ToBase64String(ms.ToArray());
+                }
+            }
+            return View();
+        }
+    }
+}
---
+++ Gerador De QrCode/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Gerador_De_QrCode.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
